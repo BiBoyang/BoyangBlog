73b17bbfb18a61d668b1a1aeebeb9ff3df0dc5c1
Boyang <biboyang0@126.com>
2020-04-30 04:53:42 +0800

    no message

---
+++ Code/property/ViewController.m
@@ -0,0 +1,28 @@
+//
+//  ViewController.m
+//  property
+//
+//  Created by 毕博洋 on 2020/4/30.
+//  Copyright © 2020 毕博洋. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@property (nonatomic, copy) NSString *Boyang;
+
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    self.Boyang = @"bby_bby";
+    
+}
+
+
+@end
